030cdb631a7775782337a4133173d9cc6742d3dc
José Lucas <jose.lukass03@gmail.com>
2021-08-12 15:49:52 -0300

    feat: create constants

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Max items per page
+ * @constant
+ */
+export const maxItemsPerPage = 30;
+
+/**
+ * Min items per page
+ * @constant
+ */
+export const minItemsPerPage = 30;
+
+/**
+ * Inital page
+ * @constant
+ */
+export const firstPage = 30;
