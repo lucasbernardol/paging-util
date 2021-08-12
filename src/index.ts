99125cf8f3ec34d65dc4d751490657711e50b7ca
José Lucas <jose.lukass03@gmail.com>
2021-08-12 16:06:12 -0300

    feat: create a method to calculate range

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,28 @@ export const minItemsPerPage = 10;
  * @constant
  */
 export const firstPage = 1;
+
+/**
+ * Calculate range
+ */
+export function range(start: number, end: number): number[] | null {
+  const argumentsLengthIsLessThanTwo = arguments.length < 2;
+
+  if (argumentsLengthIsLessThanTwo) {
+    return null;
+  }
+
+  const accumulator: number[] = [];
+
+  const accumulatorArrayLength = end - start + 1;
+
+  let interactionsIndex = 0;
+
+  while (interactionsIndex < accumulatorArrayLength) {
+    accumulator[interactionsIndex] = start + interactionsIndex;
+
+    interactionsIndex++;
+  }
+
+  return accumulator.length ? accumulator : null;
+}
