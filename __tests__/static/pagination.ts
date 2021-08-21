1f31d5d025fbbf87fa24d159744eff561e39eb02
José Lucas <jose.lukass03@gmail.com>
2021-08-21 18:02:19 -0300

    test: add static values for the second test case

--- __tests__/static/pagination.ts
+++ __tests__/static/pagination.ts
@@ -22,3 +22,25 @@ export const first = {
     results: 9,
   },
 };
+
+export const second = {
+  options: {
+    total: 100,
+    limit: 10,
+    page: 9,
+  },
+  expected: {
+    total: 100,
+    pages: 10,
+    currentPage: 9,
+    firstPage: 1,
+    limit: 10,
+    next: 10,
+    previous: 8,
+    hasNextPage: true,
+    hasPreviousPage: true,
+    firstResult: 80,
+    lastResult: 89,
+    results: 10,
+  },
+};
