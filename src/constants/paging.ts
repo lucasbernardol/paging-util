01964c2645bf3ef53dd9ff82dff5527af574cbc7
lucasbernardol <jose.lukass03@gmail.com>
2022-02-04 19:15:09 -0300

    feat: add constants

---
+++ src/constants/paging.ts
@@ -0,0 +1,14 @@
+/**
+ * @constant FIRST_PAGE
+ */
+export const FIRST_PAGE = 1;
+
+/**
+ * @constant MIN_ITEMS_PER_PAGE
+ */
+export const MIN_ITEMS_PER_PAGE = 10;
+
+/**
+ * @constant MAX_ITEMS_PER_PAGE
+ */
+export const MAX_ITEMS_PER_PAGE = 10;
